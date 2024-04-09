d6eca8015f22a7a9ac969d4fc7c83d452592ce0d
Anna <anna.briancon33@gmail.com>
2024-04-09 21:54:25 +0200

    Maj responsive

--- script.js
+++ script.js
@@ -35,3 +35,59 @@ document.addEventListener("DOMContentLoaded", function() {
         nextImage();
     });
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const nav = document.querySelector('header');
+    const banner = document.querySelector('.banner');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > banner.offsetHeight) {
+            nav.classList.add('scrolled');
+        } else {
+            nav.classList.remove('scrolled');
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    function highlightNavLink() {
+        const sections = document.querySelectorAll('section');
+        const scrollPosition = window.scrollY;
+
+        sections.forEach(section => {
+            if (scrollPosition >= section.offsetTop && scrollPosition < section.offsetTop + section.offsetHeight) {
+                const sectionId = section.getAttribute('id');
+                navLinks.forEach(link => {
+                    if (link.getAttribute('href').slice(1) === sectionId) {
+                        link.classList.add('active');
+                    } else {
+                        link.classList.remove('active');
+                    }
+                });
+            }
+        });
+    }
+
+    highlightNavLink();
+
+    window.addEventListener('scroll', highlightNavLink); 
+});
+
+
+// Affiche le menu lors du clique sur le bouton burger
+document.addEventListener('DOMContentLoaded', function () {
+    const burgerBtn = document.getElementById('burger-btn');
+    const nav = document.querySelector('nav');
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    burgerBtn.addEventListener('click', function () {
+        nav.classList.toggle('show-menu');
+    });
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            nav.classList.remove('show-menu');
+        });
+    });
+});
